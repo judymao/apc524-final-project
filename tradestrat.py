4fbff9f517c91d91afa91a19aabdbf81f093fc75
judymao <jzm1303@outlook.com>
2022-11-02 21:06:46 -0400

    Add Backtest class skeleton

--- tradestrat.py
+++ tradestrat.py
@@ -1,47 +1,82 @@
 # Imports
 
-import numpy as np
-import pandas as pd
-
-from numpy.typing import NDArray
+import abc
 from collections.abc import Callable
 
-import abc
+import numpy as np
+import pandas as pd
+from numpy.typing import NDArray
 
 
 class Strategy(abc.ABC):
-    
     def __init__(self, data: list[str] | dict[str, pd.DataFrame]) -> None:
-        ''' 
+        """
         Initialize Strategy class
-        
+
         Args:
-            data: list of tickers to be considered in universe OR 
-                  dictionary of DataFrames, each containing dates along rows and tickers along columns, 
+            data: list of tickers to be considered in universe OR
+                  dictionary of DataFrames, each containing dates along rows and tickers along columns,
                   with one DataFrame per value (e.g. data = {'price': ..., 'PE': ...})
-            
+
         Return:
             None
-        '''
-        
+        """
+
         # TODO
         ...
-        
+
     @abc.abstractmethod
     def get_weights(self) -> pd.DataFrame:
-        '''
+        """
         Get strategy weights over time
-        
+
         Args:
             ...
-            
+
         Return:
             DataFrame containing dates along rows and tickers along columns, with values being the strategy weights
-        
-        '''
-        
+
+        """
+
         ...
 
 
-        
-        
+class Backtest:
+    def __init__(
+        self,
+        strategy: Strategy,
+        rebal_period: int | None = None,
+        transaction_cost: float = 0,
+        start_date: datetime | None = None,
+        end_date: datetime | None = None,
+    ) -> None:
+        """
+        Initialize Backtest class
+
+        Args:
+            strategy:         computed strategy results
+            rebal_period:     rebalance period in days (if None, never rebalance) [default None]
+            transaction_cost: cost per transaction as a fraction of total spending [default 0]
+            start_date:       date to start backtest period (if None, start at beginning of available data) [default None]
+            end_date:         date to end backtest period (if None, end at end of available data) [default None]
+
+        Return:
+            None
+        """
+
+        # TODO
+        ...
+
+    def get_return(self, cumulative: bool = False) -> NDArray[float]:
+        """
+        Get backtested returns
+
+        Args:
+            cumulative: if True, return cumulative return [default False]
+
+        Return:
+            Array of returns over the backtest period
+
+        """
+        # TODO
+        ...

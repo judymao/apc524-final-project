784606bc4cc59e2b72c2d7e8eea9a120e26d4aae
judymao <jzm1303@outlook.com>
2022-12-04 16:57:31 -0500

    Update test_backtest.py

--- tests/test_backtest.py
+++ tests/test_backtest.py
@@ -102,23 +102,31 @@ def test_get_return():
     strat = StrategyTest(data=["AAPL", "IBM"])
     backtest = Backtest(strat)
 
-    assert type(backtest.get_return()) == np.ndarray
-    assert type(backtest.get_return(cumulative=True)) == np.ndarray
-
-    assert len(backtest.get_return()) == len(backtest.price_data)
-    assert len(backtest.get_return(cumulative=True)) == len(backtest.price_data)
-
     returns = backtest.get_return()
     cum_returns = backtest.get_return(cumulative=True)
+
+    assert type(returns) == np.ndarray
+    assert type(cum_returns) == np.ndarray
+
+    assert len(returns) == len(backtest.price_data)
+    assert len(cum_returns) == len(backtest.price_data)
+
     assert list(np.cumprod([1 + ret for ret in returns]) - 1) == list(cum_returns)
+
+    assert not np.isnan(returns).any()
+    assert not np.isnan(cum_returns).any()
+
+    assert not np.isinf(returns).any()
+    assert not np.isinf(cum_returns).any()
 
 
 def test_get_annualized_return():
     strat = StrategyTest(data=["AAPL", "IBM"])
     backtest = Backtest(strat)
 
-    assert backtest.get_annualized_return() >= -1
-    assert type(backtest.get_annualized_return()) == np.float64
+    annualized_return = backtest.get_annualized_return()
+    assert annualized_return >= -1
+    assert type(annualized_return) == np.float64
 
     assert backtest.get_annualized_return(
         daily_returns=np.array([0.001, 0.002, 0.005])
@@ -130,14 +138,20 @@ def test_get_annualized_return():
         daily_returns=np.array([-0.001, 0.002, -0.005])
     ) == pytest.approx(-0.29, abs=0.01)
 
+    assert not np.isnan(annualized_return)
+    assert not np.isinf(annualized_return)
+
 
 def test_vol():
     strat = StrategyTest(data=["AAPL", "IBM"])
     backtest = Backtest(strat)
 
     # test get_annualized_vol
-    assert type(backtest.get_annualized_vol()) == np.float64
-    assert backtest.get_annualized_vol() >= 0
+    annual_vol = backtest.get_annualized_vol()
+    assert type(annual_vol) == np.float64
+    assert annual_vol >= 0
+    assert not np.isinf(annual_vol)
+    assert not np.isnan(annual_vol)
 
     assert backtest.get_annualized_vol(
         returns=np.array([0.001, 0.002, 0.005])
@@ -174,8 +188,11 @@ def test_vol():
     assert vol_neg_scaled == pytest.approx(2 * vol_original, rel=0.001)
 
     # test get_annualized_downside_vol
-    assert type(backtest.get_annualized_downside_vol()) == np.float64
-    assert backtest.get_annualized_downside_vol() >= 0
+    annual_downside_vol = backtest.get_annualized_downside_vol()
+    assert type(annual_downside_vol) == np.float64
+    assert annual_downside_vol >= 0
+    assert not np.isinf(annual_downside_vol)
+    assert not np.isnan(annual_downside_vol)
 
     assert (
         backtest.get_annualized_downside_vol(returns=np.array([0.001, 0.002, 0.005]))
@@ -214,7 +231,8 @@ def test_sharpe():
     backtest = Backtest(strat)
 
     # test sortino
-    assert type(backtest.get_sharpe()) == np.float64
+    sharpe = backtest.get_sharpe()
+    assert type(sharpe) == np.float64
 
     # test rolling_sortino
     with pytest.raises(ValueError):
@@ -230,13 +248,17 @@ def test_sharpe():
         len(backtest.get_rolling_sharpe(lookback=10)) == len(backtest.price_data) - 10
     )
 
+    assert not np.isnan(sharpe)
+    assert not np.isinf(sharpe)
+
 
 def test_sortino():
     strat = StrategyTest(data=["AAPL", "IBM"])
     backtest = Backtest(strat)
 
     # test sortino
-    assert type(backtest.get_sortino()) == np.float64
+    sortino = backtest.get_sortino()
+    assert type(sortino) == np.float64
 
     # test rolling_sortino
     with pytest.raises(ValueError):
@@ -252,6 +274,9 @@ def test_sortino():
         len(backtest.get_rolling_sortino(lookback=10)) == len(backtest.price_data) - 10
     )
 
+    assert not np.isnan(sortino)
+    assert not np.isinf(sortino)
+
 
 def test_get_avg_underwater_time():
     strat = StrategyTest(data=["AAPL", "IBM"])
@@ -263,9 +288,13 @@ def test_get_avg_underwater_time():
     with pytest.raises(ValueError):
         backtest.get_avg_underwater_time(threshold_days=1)
 
-    assert backtest.get_avg_underwater_time() >= 0
-    assert backtest.get_avg_underwater_time() < len(strat.data.index)
-    assert type(backtest.get_avg_underwater_time()) == np.float64
+    avg_underwater_time = backtest.get_avg_underwater_time()
+    assert avg_underwater_time >= 0
+    assert avg_underwater_time < len(strat.data.index)
+    assert type(avg_underwater_time) == np.float64
+
+    assert not np.isnan(avg_underwater_time)
+    assert not np.isinf(avg_underwater_time)
 
 
 def test_get_max_underwater_time():
@@ -278,9 +307,13 @@ def test_get_max_underwater_time():
     with pytest.raises(ValueError):
         backtest.get_max_underwater_time(threshold_days=1)
 
-    assert backtest.get_max_underwater_time() >= 0
-    assert backtest.get_max_underwater_time() < len(strat.data.index)
-    assert type(backtest.get_max_underwater_time()) == int
+    max_underwater_time = backtest.get_max_underwater_time()
+    assert max_underwater_time >= 0
+    assert max_underwater_time < len(strat.data.index)
+    assert type(max_underwater_time) == int
+
+    assert not np.isnan(max_underwater_time)
+    assert not np.isinf(max_underwater_time)
 
 
 def test_get_underwater_time():
@@ -293,7 +326,11 @@ def test_get_underwater_time():
     with pytest.raises(ValueError):
         backtest.get_underwater_time(threshold_days=1)
 
-    assert type(backtest.get_underwater_time()) == tuple
+    underwater_time = backtest.get_underwater_time()
+    assert type(underwater_time) == tuple
+
+    assert not np.isnan(underwater_time).any()
+    assert not np.isinf(underwater_time).any()
 
 
 def test_get_max_consecutive():
@@ -308,10 +345,20 @@ def test_get_max_consecutive():
     assert backtest.get_max_consecutive_negative() <= len(strat.data.index)
     assert type(backtest.get_max_consecutive_negative()) == int
 
+    assert not np.isnan(backtest.get_max_consecutive_positive())
+    assert not np.isinf(backtest.get_max_consecutive_positive())
+
+    assert not np.isnan(backtest.get_max_consecutive_negative())
+    assert not np.isinf(backtest.get_max_consecutive_negative())
+
 
 def test_get_max_drawdown():
     strat = StrategyTest(data=["AAPL", "IBM"])
     backtest = Backtest(strat)
 
-    assert backtest.get_max_drawdown() >= 0
-    assert type(backtest.get_max_drawdown()) == np.float64
+    max_drawdown = backtest.get_max_drawdown()
+    assert max_drawdown >= 0
+    assert type(max_drawdown) == np.float64
+
+    assert not np.isnan(max_drawdown)
+    assert not np.isinf(max_drawdown)

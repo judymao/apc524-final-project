2f8a01660e5fc42a9639148a60271531f2cb9426
judymao <jzm1303@outlook.com>
2022-12-06 11:13:43 -0500

    Update index + pre-commit fixes

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Developing Trading Strategies in Python'
-copyright = '2022, Zhuomin Mao, Dorothy Zhang, Luis Bento'
-author = 'Zhuomin Mao, Dorothy Zhang, Luis Bento'
+project = "Developing Trading Strategies in Python"
+copyright = "2022, Zhuomin Mao, Dorothy Zhang, Luis Bento"
+author = "Zhuomin Mao, Dorothy Zhang, Luis Bento"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,13 +21,12 @@ extensions = [
     "sphinx_copybutton",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]

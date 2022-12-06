82e72d6add76d80fc8fa1d6a19f6a0341cc4b867
judymao <jzm1303@outlook.com>
2022-12-06 12:37:03 -0500

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -19,10 +19,3 @@ def docs(session: nox.Session) -> None:
     session.install(".[docs]")
     session.chdir("docs")
     session.run("sphinx-build", "-M", "html", ".", "build")
-
-
-@nox.session
-def serve(session: nox.Session) -> None:
-    docs(session)
-    print("Launching docs at http://localhost:8000/ - use Ctrl-C to quit")
-    session.run("python", "-m", "http.server", "8000", "-d", "_build/html")
